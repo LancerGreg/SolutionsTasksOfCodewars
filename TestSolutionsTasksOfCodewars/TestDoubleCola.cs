b28e797d23c2e1dfabe14560683f8070161da6c2
Roman <vrv2323@gmail.com>
2020-01-29 14:49:26 +0200

    DoubleCola

---
+++ TestSolutionsTasksOfCodewars/TestDoubleCola.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using SolutionsTasksOfCodewars.Tasks;
+using System;
+
+namespace TestSolutionsTasksOfCodewars
+{
+    [TestFixture]
+    public class TestDoubleCola
+    {
+        [Test]
+        public void Test1()
+        {
+            string[] names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
+            int n = 1;
+            Assert.AreEqual("Sheldon", DoubleCola.WhoIsNext(names, n));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string[] names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
+            int n = 6;
+            Assert.AreEqual("Sheldon", DoubleCola.WhoIsNext(names, n));
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            string[] names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
+            int n = 6;
+            Assert.AreEqual("Penny", DoubleCola.WhoIsNext(names, 52));
+        }
+        
+        [Test]
+        public void Test4()
+        {
+            string[] names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
+            int n = 6;
+            Assert.AreEqual("Leonard", DoubleCola.WhoIsNext(names, 7230702951));
+        }
+    }
+}
