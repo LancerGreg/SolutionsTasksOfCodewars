099bd9fb3a114ff78ff743fccf79d4a8a2ea9b33
Roman <vrv2323@gmail.com>
2020-01-31 16:48:01 +0200

    Anagrams

---
+++ TestSolutionsTasksOfCodewars/TestKata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestSolutionsTasksOfCodewars
+{
+    using NUnit.Framework;
+    using SolutionsTasksOfCodewars.Tasks;
+    using System;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class TestKata
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new List<string> { "a" }, Kata.Anagrams("a", new List<string> { "a", "b", "c", "d" }));
+            Assert.AreEqual(new List<string> { "carer", "arcre", "carre" }, Kata.Anagrams("racer", new List<string> { "carer", "arcre", "carre", "racrs", "racers", "arceer", "raccer", "carrer", "cerarr" }));
+            Assert.AreEqual(new List<string> { "aabb", "bbaa" }, Kata.Anagrams("abba", new List<string> { "aabb", "abcd", "bbaa", "dada" }));
+            Assert.AreEqual(new List<string>(), Kata.Anagrams("laser", new List<string> { "lazing", "lazy", "lacer" }));
+        }
+    }
+}
