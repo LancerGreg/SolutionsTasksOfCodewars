be7b131875eb1d3581a27744be3bf95bbf84375e
Roman <vrv2323@gmail.com>
2020-02-03 13:32:18 +0200

    MergedStringChecker

---
+++ SolutionsTasksOfCodewars/Tasks/MergedStringChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SolutionsTasksOfCodewars.Tasks
+{
+    public class MergedStringChecker
+    {
+        public static bool isMerge(string s, string part1, string part2)
+        {
+            bool empty1 = part1.Length == 0,
+                 empty2 = part2.Length == 0,
+                 works1 = false,
+                works2 = false;
+
+            if (s.Length == 0)
+            {
+                if (part1.Length == 0 && part2.Length == 0) return true;
+                return false;
+            }
+            else
+            {
+                if (!empty1 && s[0] == part1[0]) works1 = isMerge(s.Substring(1), part1.Substring(1), part2);
+                if (!empty2 && s[0] == part2[0]) works2 = isMerge(s.Substring(1), part1, part2.Substring(1));
+                return works1 || works2;
+            }
+        }
+    }
+}
---
+++ TestSolutionsTasksOfCodewars/TestMergedStringChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using SolutionsTasksOfCodewars.Tasks;
+
+namespace TestSolutionsTasksOfCodewars
+{
+    [TestFixture]
+    public class TestMergedStringChecker
+    {
+		[Test]
+		public void HappyPath1()
+		{
+			Assert.IsTrue(MergedStringChecker.isMerge("codewars", "code", "wars"), "codewars can be created from code and wars");
+		}
+
+		[Test]
+		public void HappyPath2()
+		{
+			Assert.IsTrue(MergedStringChecker.isMerge("codewars", "cdwr", "oeas"), "codewars can be created from cdwr and oeas");
+		}
+
+		[Test]
+		public void SadPath1()
+		{
+			Assert.IsFalse(MergedStringChecker.isMerge("codewars", "cod", "wars"), "Codewars are not codwars");
+		}
+	}
+}
