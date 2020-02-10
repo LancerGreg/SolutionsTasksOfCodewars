fe89420c9d31b457049a1341ef6d6bf1618c5b3c
Roman <vrv2323@gmail.com>
2020-02-10 12:13:11 +0200

    Finder

---
+++ TestSolutionsTasksOfCodewars/TestFinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using SolutionsTasksOfCodewars.Tasks;
+
+namespace TestSolutionsTasksOfCodewars
+{
+    public class TestFinder
+    {
+        [Test]
+        public void sampleTests()
+        {
+
+            string a = ".W.\n" +
+                       ".W.\n" +
+                       "...",
+
+                   b = ".W.\n" +
+                       ".W.\n" +
+                       "W..",
+
+                   c = "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       "......",
+
+                   d = "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       ".....W\n" +
+                       "....W.";
+
+            Assert.AreEqual(true, Finder.PathFinder(a));
+            Assert.AreEqual(false, Finder.PathFinder(b));
+            Assert.AreEqual(true, Finder.PathFinder(c));
+            Assert.AreEqual(false, Finder.PathFinder(d));
+        }
+    }
+}
