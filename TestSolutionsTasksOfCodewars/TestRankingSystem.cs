2a5ce96fb398a6f91c03a0589af74824a1a60c47
Roman <vrv2323@gmail.com>
2020-02-11 12:44:21 +0200

    RankingSystem

---
+++ TestSolutionsTasksOfCodewars/TestRankingSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using SolutionsTasksOfCodewars.Tasks;
+
+namespace TestSolutionsTasksOfCodewars
+{
+    [TestFixture]
+    public class TestRankingSystem
+    {
+        [Test]
+        public void MyTest1()
+        {
+            User user = new User();
+            user.incProgress(-7);
+            Assert.AreEqual(10, user.Progress);
+            Assert.AreEqual(-8, user.Rank);
+        }
+
+        [Test]
+        public void MyTest2()
+        {
+            User user = new User();
+            user.incProgress(-5);
+            Assert.AreEqual(90, user.Progress);
+            Assert.AreEqual(-8, user.Rank);
+        }
+
+        [Test]
+        public void MyTest3()
+        {
+            User user = new User();
+            user.incProgress(-7);
+            user.incProgress(-5);
+            Assert.AreEqual(0, user.Progress);
+            Assert.AreEqual(-7, user.Rank);
+        }
+
+
+    }
+}
